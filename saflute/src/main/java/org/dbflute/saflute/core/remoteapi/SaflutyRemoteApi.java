587812b5596401868e13978c8f3e634d53c7d720
jflute <dbflute@gmail.com>
2017-10-04 02:29:14 +0900

    making remoteapi for saflute

---
+++ saflute/src/main/java/org/dbflute/saflute/core/remoteapi/SaflutyRemoteApi.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.dbflute.saflute.core.remoteapi;
+
+import java.util.function.Consumer;
+
+import org.dbflute.remoteapi.FlutyRemoteApi;
+import org.dbflute.remoteapi.FlutyRemoteApiRule;
+import org.dbflute.saflute.core.util.Sato;
+import org.dbflute.saflute.web.servlet.request.RequestManager;
+
+/**
+ * @author jflute
+ */
+public class SaflutyRemoteApi extends FlutyRemoteApi {
+
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    protected RequestManager requestManager; // not null after set, for validation and various purpose
+
+    // ===================================================================================
+    //                                                                         Constructor
+    //                                                                         ===========
+    public SaflutyRemoteApi(Consumer<FlutyRemoteApiRule> defaultOpLambda, Object callerExp) {
+        super(defaultOpLambda, callerExp);
+    }
+
+    public void acceptRequestManager(RequestManager requestManager) {
+        this.requestManager = requestManager;
+    }
+
+    // ===================================================================================
+    //                                                                      RemoteApi Rule
+    //                                                                      ==============
+    @Override
+    protected FlutyRemoteApiRule newRemoteApiRule() {
+        return new SaflutyRemoteApiRule();
+    }
+
+    // ===================================================================================
+    //                                                                      Error Handling
+    //                                                                      ==============
+    @Override
+    protected String convertBeanToDebugString(Object bean) {
+        return Sato.string(bean); // because its toString() may not be overridden
+    }
+}
